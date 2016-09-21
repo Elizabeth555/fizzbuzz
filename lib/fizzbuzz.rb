aa8014d8547f8c13ae9819db43a7fd3c1153fd61
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-09-21 21:08:51 +0100

    fizzbuzz'

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz (number)
+
+ if number%3 ==0 && number%5 ==0
+   puts 'fizzbuzz'
+ elsif number%5 == 0
+   puts 'buzz'
+ elsif number%3 == 0
+   puts 'fizz'
+ else puts number
+    end
+end
+
